92a0130f22b55ec6e128ca0af10de50650580349
YE GAO <sortsimilar@gmail.com>
2022-07-04 18:44:06 +0800

    update

--- losses/KD.py
+++ losses/KD.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class DistillKL(nn.Module):
     def __init__(self, temp_factor):
         super(DistillKL, self).__init__()
@@ -10,7 +11,7 @@ class DistillKL(nn.Module):
         self.kl_div = nn.KLDivLoss(reduction="none")
 
     def forward(self, input, target):
-        log_p = torch.log_softmax(input/self.temp_factor, dim=1)
-        q = torch.softmax(target/self.temp_factor, dim=1)
-        loss = self.kl_div(log_p, q)*(self.temp_factor**2)/input.size(0)
+        log_p = torch.log_softmax(input / self.temp_factor, dim=1)
+        q = torch.softmax(target / self.temp_factor, dim=1)
+        loss = self.kl_div(log_p, q) * (self.temp_factor ** 2) / input.size(0)
         return loss
