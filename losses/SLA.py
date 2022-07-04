780ad031aa3eb78e06c7b0842a5542d28b76cc20
YE GAO <sortsimilar@gmail.com>
2022-07-04 18:33:09 +0800

    update

--- losses/SLA.py
+++ losses/SLA.py
@@ -7,8 +7,10 @@ import torch.nn.functional as F
 from .FL import FTLoss
 from .KD import DistillKL
 
+
 class SemCKDLoss(nn.Module):
     """Cross-Layer Distillation with Semantic Calibration, AAAI2021"""
+
     def __init__(self, args):
         super(SemCKDLoss, self).__init__()
         self.criterion_kl = DistillKL(args.temp)
@@ -16,16 +18,15 @@ class SemCKDLoss(nn.Module):
         self.crit = nn.MSELoss(reduction='none')
         self.alpha = args.alpha
 
-        
     def forward(self, s_value, f_target, weight):
         bsz, num_stu = weight.shape
         ind_loss_kd = torch.zeros(bsz, num_stu).cuda()
         ind_loss_fl = torch.zeros(bsz, num_stu).cuda()
 
         for i in range(num_stu):
-            ind_loss_kd[:, i] = self.criterion_kl(s_value[i], f_target).reshape(bsz,-1).mean(-1)
-            ind_loss_fl[:, i] = self.criterion_fl(s_value[i], f_target).reshape(bsz,-1).mean(-1)
-            
+            ind_loss_kd[:, i] = self.criterion_kl(s_value[i], f_target).reshape(bsz, -1).mean(-1)
+            ind_loss_fl[:, i] = self.criterion_fl(s_value[i], f_target).reshape(bsz, -1).mean(-1)
+
         loss_kd = (weight * ind_loss_kd).sum()
         loss_fl = (weight * ind_loss_fl).sum()
         return loss_kd + self.alpha * loss_fl
